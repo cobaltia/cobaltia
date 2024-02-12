170847c989a06719ca131f3fedd74f2e81d31b49
JuanPablo2655 <personal@isidrocasiano.com>
2024-02-12 01:25:11 -0500

    chore: add todo

--- src/lib/setup/redis.ts
+++ src/lib/setup/redis.ts
@@ -3,6 +3,7 @@ import Redis from 'ioredis';
 import { REDIS_URI } from '#root/config';
 
 container.redis = new Redis(REDIS_URI);
+// TODO(*): Figure out how to flush the Redis database when the program exits.
 await container.redis.flushall();
 
 declare module '@sapphire/framework' {
