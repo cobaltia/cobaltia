12b5cfc7fd7246c995a9af165e9aa6994302e616
JuanPablo2655 <personal@isidrocasiano.com>
2024-02-17 13:38:56 -0500

    feat(voiceExp): change dm message to channel message

--- src/listeners/experience/voiceExperience.ts
+++ src/listeners/experience/voiceExperience.ts
@@ -1,9 +1,10 @@
 import type { User as PrismaUser } from '@prisma/client';
+import { isTextBasedChannel } from '@sapphire/discord.js-utilities';
 import { Listener, Result } from '@sapphire/framework';
 import { DurationFormatter, Time } from '@sapphire/time-utilities';
 import { isNullish, roundNumber } from '@sapphire/utilities';
 import { bold, type GuildMember, type VoiceState } from 'discord.js';
-import { getUser } from '#lib/database';
+import { getGuild, getUser } from '#lib/database';
 import { Events } from '#lib/types';
 import { addBonus, formatMoney } from '#util/common';
 import { calculateBonus } from '#util/discord-utilities';
@@ -27,8 +28,18 @@ export class VoiceExperienceListener extends Listener<typeof Events.VoiceChannel
 	}
 
 	private async handleOk(member: GuildMember, previous: VoiceState, start: string | null, data: PrismaUser) {
+		const { guild } = member;
+		const guildResult = await Result.fromAsync(async () => getGuild(member.id));
+		if (guildResult.isErr()) throw guildResult.unwrapErr();
+		const { voiceChannelId } = guildResult.unwrap();
+		if (!voiceChannelId) return;
+		const channel = guild.channels.cache.get(voiceChannelId);
+		if (!isTextBasedChannel(channel)) return this.handleErr(new Error('Welcome channel is not a text channel'));
+
 		if (isNullish(start))
-			return member.send("I must have been restarted while you were in a voice channel. It's so over....");
+			return channel.send(
+				`${member}, I must have been restarted while you were in a voice channel. It's so over....`,
+			);
 
 		const formatter = new DurationFormatter();
 		const message = [];
@@ -66,8 +77,10 @@ export class VoiceExperienceListener extends Listener<typeof Events.VoiceChannel
 			},
 		});
 
-		message.push(`You have earned ${bold(formatMoney(total)!)} for spending ${formatter.format(elapsed)} in VC.`);
-		await member.send({ content: message.join('\n') });
+		message.push(
+			`${member}, You have earned ${bold(formatMoney(total)!)} for spending ${formatter.format(elapsed)} in VC.`,
+		);
+		await channel.send({ content: message.join('\n') });
 	}
 
 	private async handleErr(error: unknown) {
