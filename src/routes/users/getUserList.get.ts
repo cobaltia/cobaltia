866c21e3d04ba4c56835d872aea91f1bfbd9e924
JuanPablo2655 <personal@isidrocasiano.com>
2025-05-25 20:22:52 -0400

    chore: remove leftover console log

--- src/routes/users/getUserList.get.ts
+++ src/routes/users/getUserList.get.ts
@@ -4,7 +4,6 @@ import { type FlattenedUser, flattenUser } from '#lib/api/ApiTransformers';
 export class UserRoute extends Route {
 	public async run(request: Route.Request, response: Route.Response) {
 		const { users } = request.query;
-		console.log(request.query);
 		const { client } = this.container;
 		const userList: FlattenedUser[] = [];
 		for (const user of (users as string).split(',')) {
